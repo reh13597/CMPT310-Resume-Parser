40c36455ac7b20b4a93a5553d5aa68f5e8262231
burningfiremoon <charleszhang4@gmail.com>
2025-10-24 03:18:24 -0700

    Finished cosine similarity, and top n jobs

--- src/vectorization.py
+++ src/vectorization.py
@@ -5,12 +5,13 @@
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # ============================================================
 # Load the datasets
 # ============================================================
-resumes_df = pd.read_csv("datasets\cleaned\cleaned_resumes.csv")
-jobs_df = pd.read_csv("datasets\cleaned\cleaned_job_postings.csv")
+resumes_df = pd.read_csv("datasets/cleaned/cleaned_resumes.csv")
+jobs_df = pd.read_csv("datasets/cleaned/cleaned_job_postings.csv")
 
 # ============================================================
 # Combine the combined_text fields from both datasets for fitting
@@ -43,3 +44,41 @@ jobs_tfidf = vectorizer.transform(jobs_df['combined_text'])
 print("TF-IDF fitted vocabulary size:", len(vectorizer.get_feature_names_out()))
 print("Resumes TF-IDF matrix size:", resumes_tfidf.shape)
 print("Jobs TF-IDF matrix size:", jobs_tfidf.shape)
+
+
+"""
+Cosine Similarity
+output:
+
+"""
+
+def cosineSimilarity(jobMatrix, resumeMatrix):
+    similarityMatrix = cosine_similarity(resumeMatrix, jobMatrix)
+
+    return(similarityMatrix, similarityMatrix.shape)
+
+test, shape = cosineSimilarity(jobs_tfidf,  resumes_tfidf)
+
+df = pd.DataFrame(
+    test,
+    index= [f"Resume {i}" for i in range(resumes_tfidf.shape[0])],
+    columns=[f"Job {j}" for j in range(jobs_tfidf.shape[0])]
+)
+
+print(df) # similarity matrix in a dataframe
+print("Shape:", shape)
+
+"""
+    Top N jobs for resume using a lambda function
+"""
+
+# Argument for N here
+N = 5
+
+# Gives top 5 jobs per resume || Gives top 5 resumes per job
+scoresWithJobNames = lambda x: x.nlargest(N).to_dict()
+
+# axis 1 for top jobs for resumes, axis 0 for top resumes for job
+top_matches = df.apply(scoresWithJobNames, axis=1)
+
+print(top_matches)
