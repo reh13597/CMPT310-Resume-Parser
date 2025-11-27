75bb77c7a158e41f262b10a97f35809e998e55a0
reh13597 <alexlguo18@gmail.com>
2025-11-27 13:47:18 -0800

    changed the run paths for run_pipeline.py

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -9,17 +9,17 @@ def run(script):
 
 if __name__ == "__main__":
     print("\n Starting Full Resume–Job Fit Pipeline\n")
-    
+
     # 1. Data preproccessing
     # run("data_preprocessing.py")    # Optional to run because running time is slow
 
-    # 2. Train Fit / No Fit model 
-    run("new_scripts/train_fit_model.py")
+    # 2. Train Fit / No Fit model
+    run("src/train_fit_model.py")
 
     # 3. Generate Recommendations
-    run("new_scripts/recommend_jobs.py")
+    run("src/recommend_jobs.py")
 
     # 4. Generate Visualizations
-    run("new_scripts/generate_visualizations.py")
+    run("src/generate_visualizations.py")
 
     print("\n Pipeline Complete! All results saved in datasets/ & visualizations/")
